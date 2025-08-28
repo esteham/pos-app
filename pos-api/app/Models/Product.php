fabf0b43ee1eefcf327082da005922d383e8bb26
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-29 01:18:22 +0600

    added some fillable column and function category functionality

--- pos-api/app/Models/Product.php
+++ pos-api/app/Models/Product.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'image',
+        'sku',
+        'barcode',
+        'unit',
+        'stock',
+        'vat_percent',
+        'is_active'
+    ];
+
+    public function setBarcodeAttribute(){
+        $this->attributes['barcode'] = ($value === '' ? null : $value);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
 }
