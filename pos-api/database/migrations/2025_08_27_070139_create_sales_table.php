df5d6e63ba63f3cf0a983cc24d431ab8c590b459
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-29 00:42:45 +0600

    updated migration tables comlumn

--- pos-api/database/migrations/2025_08_27_070139_create_sales_table.php
+++ pos-api/database/migrations/2025_08_27_070139_create_sales_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->string('invoice_number')->unique();
+            $table->string('invoice_no')->unique();
             $table->foreignId('customer_id')->constrained()->nullOnDelete();
             $table->string('customer_name')->nullable();
             $table->string('customer_phone')->nullable();
             $table->foreignId('sold_by')->constrained('users')->cascadeOnDelete();
-            $table->decimal('subtotal', 10, 2);
-            $table->decimal('total_amount', 10, 2);
+            $table->decimal('subtotal', 10, 2)->default(0);
             $table->decimal('discount', 10, 2)->default(0);
-            $table->decimal('grand_total', 10, 2);
+            $table->decimal('grand_total', 10, 2)->default(0);
+            $table->decimal('paid_ammount',10, 2)->default(0);
             $table->enum('payment_method', ['cash', 'credit_card', 'internet_banking', 'bank_transfer'])->default('cash');
             $table->enum('status', ['pending', 'completed', 'canceled'])->default('pending');
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
