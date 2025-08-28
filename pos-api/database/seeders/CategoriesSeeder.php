1af936699c91a684ee4ba143aa8591af793dce61
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-29 02:13:32 +0600

    added category name in categories tabel

--- pos-api/database/seeders/CategoriesSeeder.php
+++ pos-api/database/seeders/CategoriesSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategoriesSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class CategoriesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        foreach(['Groceery', 'Dairy', 'Beverages', 'Personal Care'] as $name)
+        {
+            Category::updateOrCreate(
+
+                ['name' => $name],
+                ['slug' => Str::slug($name)]
+            
+            );
+        }
     }
 }
