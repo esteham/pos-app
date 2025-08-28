f71e9f411f12d682270de9fd9229d2c84a7a1758
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-29 02:28:20 +0600

    added customer data in customers tabel

--- pos-api/database/seeders/CustomersSeeder.php
+++ pos-api/database/seeders/CustomersSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class CustomersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $list = [
+
+        	['name' => 'Rahim', 'phone' => '01711458777','email' =>'rahim@gmail.com', 'address' => 'Dhanmondi'],
+        	['name' => 'Monir', 'phone' => '01819874521','email' =>'monir@gmail.com', 'address' => 'Mirpur'],
+        ];
+
+        foreach($list as $c) Customer::updateOrCreate(['phone' => $c['phone']], $c);
     }
 }
