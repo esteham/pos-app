8cff677f9bdb18354ecc065e6cafcec7f8d805fe
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-29 01:48:54 +0600

    added some fillable column

--- pos-api/app/Models/SaleItem.php
+++ pos-api/app/Models/SaleItem.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class SaleItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sale_id',
+        'product_id',
+        'product_name',
+        'unit_price',
+        'quantity',
+        'unit',
+        'discount',
+        'vat_percent',
+        'vat_amount',
+        'total_price'
+    ];
+
 }
