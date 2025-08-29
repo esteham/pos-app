9ef58396aa6902c59d0ae78db5fe40a0de92d710
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-29 15:26:55 +0600

    create Authcontoller for auth login and logout function with username phone and email

---
+++ pos-api/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'login'   => 'required|string',
+            'password'=> 'required|string|min:6',
+        ]);
+
+        $login = $data['login'];
+
+        //Login fetch
+        if (filter_var($login, FILTER_VALIDATE_EMAIL)){
+            $user = User::where('email', $login)->first();
+        }
+        elseif (is_numeric($login)){
+            $user = User::where('phone', $login)->first();
+        }
+        else {
+            $user = User::where('username', $login)->first();
+        }
+
+        //if not user or password
+        if (!$user || !Hash::check($data['password'], $user->password)){
+            return response()->json(['message' => 'Invalid Credentials'], 401);
+        }
+
+        //Create token
+        $tokenResult = $user->createToken('pos-token');
+        $token = $tokenResult->plainTextToken;
+
+        //Optional (token expiry set)
+        // $user->tokens()->latest()->first()->update([
+        //     'expires_at' => now()->addHours(24),
+        // ]);
+
+
+        return response()->json([
+            'token' => $token,
+            'user'  => [
+                    'id'    => $user->id,
+                    'name'  => $user->name,
+                    'username'=>$user->username,
+                    'phone' => $user->phone,
+                    'email' => $user->email,
+                ]
+        ]);
+
+    }
+
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
+}
