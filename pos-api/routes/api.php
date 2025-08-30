859e8aebfc71165f60ba67d3b9be84f0e76e0df6
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-30 21:46:41 +0600

    install breeze

--- pos-api/routes/api.php
+++ pos-api/routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-  
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,22 +9,11 @@ use App\Http\Controllers\Api\ProductController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::post('/login', [AuthController::class, 'login']);
-
-//Protected routes (only for token users)
-Route::middleware(['auth:sanctum'])->group(function (){
-    Route::get('/user', function (Request $request){
-        return $request->user();
-    });
-
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-Route::match(['get', 'post'], '/products/search', [ProductController::class, 'search']);
-Route::match(['get', 'post'], '/products/find', [ProductController::class, 'find']);
-
